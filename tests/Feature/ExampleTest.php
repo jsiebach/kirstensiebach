b498e7324115fa7c276daeaab717a8abaaf84664
Jeff Siebach <jsiebach@gmail.com>
2021-04-15 22:39:50 -0500

    actions test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,15 +7,25 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->seed();
+    }
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_the_homepage_loads()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        $response->assertSeeText('Martian Geologist');
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
