27d754e67c44621b4e8c2a27484f7acac8ecbe1a
Jeff Siebach <jsiebach@gmail.com>
2021-04-19 20:24:48 -0500

    Dockerized

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ docker-compose -f docker-compose.prod.yml pull
 # Relaunch with new docker images
 docker-compose -f docker-compose.prod.yml up -d
 
+# Cache the config
+docker-compose -f docker-compose.prod.yml exec app php artisan config:cache
+
 # Migrate the database
 docker-compose -f docker-compose.prod.yml exec app php artisan migrate --force
