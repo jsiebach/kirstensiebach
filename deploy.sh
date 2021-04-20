cc305356cba764b775959364565d25c325191e7a
Jeff Siebach <jsiebach@gmail.com>
2021-04-19 20:47:48 -0500

    T flag

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-export COMPOSE_INTERACTIVE_NO_CLI=1
 
 # Fetch new docker images
 docker-compose -f docker-compose.prod.yml pull
@@ -8,7 +7,7 @@ docker-compose -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.prod.yml up -d
 
 # Cache the config
-docker-compose -f docker-compose.prod.yml exec app php artisan config:cache
+docker-compose -f docker-compose.prod.yml exec -T app php artisan config:cache
 
 # Migrate the database
-docker-compose -f docker-compose.prod.yml exec app php artisan migrate --force
+docker-compose -f docker-compose.prod.yml exec -T app php artisan migrate --force
