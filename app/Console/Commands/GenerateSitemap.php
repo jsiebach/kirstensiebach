3c34a889370fb22724445cd2cf206742dfff677d
Jeff Siebach <jsiebach@gmail.com>
2024-01-05 10:40:43 -0700

    Sitemap command

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Page;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate a sitemap';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Sitemap::create('https://kirstensiebach.com')
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
